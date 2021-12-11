a310963a5fe80642aee5f288cbdf440c05241b26
berlicon <berlicon@mail.ru>
2021-12-11 20:49:23 +0300

    deleting photos + update libs

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../services/photo.service';
+import { Photo, PhotoService } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -9,7 +10,8 @@ import { PhotoService } from '../services/photo.service';
 export class Tab2Page implements OnInit {
   date = 'Date';
 
-  constructor(public photoService: PhotoService) { }
+  constructor(public photoService: PhotoService,
+    public actionSheetController: ActionSheetController) { }
 
   async ngOnInit() {
     await this.photoService.loadSaved();
@@ -26,5 +28,27 @@ export class Tab2Page implements OnInit {
 
   onDate() {
     this.date = new Date().toString();
+  }
+
+  public async showActionSheet(photo: Photo, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
   }
 }
