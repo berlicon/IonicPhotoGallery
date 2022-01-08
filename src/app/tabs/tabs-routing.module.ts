e5f87ee2c247f96dde553b1f01c1adf8fa9093b0
berlicon <berlicon@mail.ru>
2022-01-08 13:17:00 +0300

    show tab4 by default instead of tab1. time of startup 30sec->5sec. update app on Firebase. PWA app does not flick when hit button "log acceleration"

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -25,14 +25,14 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/tab4',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/tab4',
     pathMatch: 'full'
   }
 ];
