ec8c7ce3426babe5e3af19246aa848fa1afdd6f4
berlicon <berlicon@mail.ru>
2022-01-08 16:22:53 +0300

    use TF model. Donwngrage "@tensorflow/tfjs": "^3.12.0" -> 3.8.0; import "long" - fix Error: node_modules/@tensorflow/tfjs-core/dist/hash_util.d.ts:2:49 - error TS2304: Cannot find name 'Long'. - see https://github.com/tensorflow/tfjs/issues/5584 error with fresh Angular v12

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import * as tf from '@tensorflow/tfjs';
 
 
 @Component({
@@ -10,5 +11,25 @@ import { HttpClient } from '@angular/common/http';
 export class Tab5Page {
 
   constructor(private http: HttpClient) {}
+
+  testModel() {
+    // Define a model for linear regression.
+    const model = tf.sequential();
+    model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+
+    // Prepare the model for training: Specify the loss and the optimizer.
+    model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
+    // Generate some synthetic data for training.
+    const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+    const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+
+    // Train the model using the data.
+    model.fit(xs, ys).then(() => {
+      // Use the model to do inference on a data point the model hasn't seen before:
+      let x = model.predict(tf.tensor2d([5], [1, 1])).toString();
+      alert(x);
+    });
+  }
 
 }
