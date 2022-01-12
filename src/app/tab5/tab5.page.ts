4376eec1ef275f95aac1da658311f1200f94bc84
berlicon <berlicon@mail.ru>
2022-01-12 23:19:29 +0300

    face api works. But tf models not work. Migrated @tensorflow/tfjs 3.8.0->3.12.0 in order to correct run face-api, But now not work model. Error: hash_util.d.ts:3:69 - error TS2304: Cannot find name Long. Cases: 1. TF models predict work. Face api not work: @tensorflow/tfjs 3.8.0 2. TF models predict not work. Face api work: @tensorflow/tfjs 3.12.0

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import * as tf from '@tensorflow/tfjs';
-
+//import * as tf from '@tensorflow/tfjs';
+//import * as canvas from 'canvas';
+import * as faceapi from 'face-api.js';
+//import '@tensorflow/tfjs-node';
 
 @Component({
   selector: 'app-tab5',
@@ -9,10 +11,32 @@ import * as tf from '@tensorflow/tfjs';
   styleUrls: ['tab5.page.scss']
 })
 export class Tab5Page {
+  @ViewChild('inputImg') inputImg:ElementRef;
+  @ViewChild('overlay') overlay:ElementRef;
+  @ViewChild('myFileUpload') myFileUpload:ElementRef;
+  
+  SSD_MOBILENETV1 = 'ssd_mobilenetv1';
+  TINY_FACE_DETECTOR = 'tiny_face_detector';
+  selectedFaceDetector = this.SSD_MOBILENETV1;
+  
+  // ssd_mobilenetv1 options
+  minConfidence = 0.5;
 
-  constructor(private http: HttpClient) {}
+  // tiny_face_detector options
+  inputSize = 512;
+  scoreThreshold = 0.5;
+  
+  constructor(private http: HttpClient) {
+    // const { Canvas, Image, ImageData } = canvas
+    // faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
+
+    this.changeFaceDetector();
+  }
 
   testModel() {
+    debugger;
+    alert('Not work. Miggrated @tensorflow/tfjs 3.8.0->3.12.0 in order to correct run face-api, But now not work model. Error: hash_util.d.ts:3:69 - error TS2304: Cannot find name Long');
+    /*
     // Define a model for linear regression.
     const model = tf.sequential();
     model.add(tf.layers.dense({units: 1, inputShape: [1]}));
@@ -29,15 +53,61 @@ export class Tab5Page {
       // Use the model to do inference on a data point the model hasn't seen before:
       let x = model.predict(tf.tensor2d([5], [1, 1])).toString();
       alert(x);
-    });
+    });*/
   }
 
-  loadImage() {
-    alert('loadImage');
+  async uploadImage() {
+    //alert('loadImage');
+    const imgFile = this.myFileUpload.nativeElement.files[0];
+    // create an HTMLImageElement from a Blob
+    const img = await faceapi.bufferToImage(imgFile)
+    this.inputImg.nativeElement.src = img.src;
+
+    this.updateResults();
   }
 
   faceDetect() {
-    alert('faceDetect');
+    //alert('faceDetect');
+    this.updateResults();
   }
 
+  async updateResults() {
+    if (!this.isFaceDetectionModelLoaded()) {
+      return
+    }
+
+    const inputImgEl = this.inputImg.nativeElement;
+    const options = this.getFaceDetectorOptions()
+
+    const results = await faceapi.detectAllFaces(<any>inputImgEl, options)
+
+    const canvas = this.overlay.nativeElement;
+    faceapi.matchDimensions(<any>canvas, <any>inputImgEl)
+    faceapi.draw.drawDetections(<any>canvas, faceapi.resizeResults(results, <any>inputImgEl))
+  } 
+
+  isFaceDetectionModelLoaded() {
+    return !!this.getCurrentFaceDetectionNet().params
+  }
+
+  getCurrentFaceDetectionNet() {
+    if (this.selectedFaceDetector === this.SSD_MOBILENETV1) {
+      return faceapi.nets.ssdMobilenetv1
+    }
+    if (this.selectedFaceDetector === this.TINY_FACE_DETECTOR) {
+      return faceapi.nets.tinyFaceDetector
+    }
+  }
+
+  getFaceDetectorOptions() {
+    return this.selectedFaceDetector === this.SSD_MOBILENETV1
+      ? new faceapi.SsdMobilenetv1Options({ minConfidence: this.minConfidence })
+      : new faceapi.TinyFaceDetectorOptions({ inputSize: this.inputSize, scoreThreshold: this.scoreThreshold })
+  }
+
+  async changeFaceDetector() {
+    if (!this.isFaceDetectionModelLoaded()) {
+      await this.getCurrentFaceDetectionNet().load('/assets/weights')
+    }
+  }
 }
